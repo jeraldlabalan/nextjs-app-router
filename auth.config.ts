387353ff8a14488d7488741050cf956f3821f1e5
jerald <jeraldlabalan26@gmail.com>
2026-08-05 20:12:52 +0800

    feat: authentication and authorization features, added login page

---
+++ auth.config.ts
@@ -0,0 +1,24 @@
+// This file is for NextAuth
+
+import type { NextAuthConfig } from 'next-auth';
+ 
+export const authConfig = {
+  pages: {
+    signIn: '/login',
+  },
+  // Prevent users from accessing the dashboard pages unless they are logged in
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false; // Redirect unauthenticated users to login page
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL('/dashboard', nextUrl));
+      }
+      return true;
+    },
+  },
+  providers: [], // Add providers with an empty array for now
+} satisfies NextAuthConfig;
